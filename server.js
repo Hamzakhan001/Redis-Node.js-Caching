dbd02e4c601bedc539d46669eb85c2ebc96a6b64
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-03-27 19:50:30 +0500

    get species data

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ const port = process.env.PORT || 3000;
 
 
 
-async function fetchApiData(species) {
+ async function fetchApiData(species) {
 	const apiResponse = await axios.get(
 	  `https://www.fishwatch.gov/api/species/${species}`
 	);
 	console.log("Request sent to the API");
 	return apiResponse.data;
+  }
+
+  async function getSpeciesData(req, res) {
+	const species = req.params.species;
+	let results;
+  
+	try {
+	  results = await fetchApiData(species);
+	} catch (error) {
+	  console.error(error);
+	  res.status(404).send("Data unavailable");
+	}
   }
 
 
