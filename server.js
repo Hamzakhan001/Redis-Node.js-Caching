8051fcfabd70c151b9b3a9f3746e24851c31239f
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-03-27 21:24:24 +0500

    saving data into cache

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ let isCached = false;
 		if (results.length === 0) {
 		  throw "API returned an empty array";
 		}
-		
+		await redisClient.set(species, JSON.stringify(results));
 	 }
 	 res.send({
 		fromCache: false,
