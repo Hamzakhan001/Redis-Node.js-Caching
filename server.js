9eff9773b4658720f9a606432547e88a7b0b0294
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-03-27 21:23:19 +0500

    getting data from cache

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const axios = require("axios");
+const redis = require("redis");
 const app = express();
 const port = process.env.PORT || 3000;
 
+let redisClient;
+let isCached = false;
 
+(async ()=>{
+	redisClient=redis.createClient();
+	redisClient.on("error",(error)=>{
+		console.error(`Error :${error}`)
+	})
+	await redisClient.connect();
+})
 
  async function fetchApiData(species) {
 	const apiResponse = await axios.get(
@@ -18,14 +28,23 @@ const port = process.env.PORT || 3000;
 	let results;
   
 	try {
-	  results = await fetchApiData(species);
-	  if (results.length === 0) {
-		throw "API returned an empty array";
-	  }
-	  res.send({
+	 const cacheResults=await redisClient.get(species);
+	 if(cacheResults){
+		isCached=true;
+		results=JSON.parse(cacheResults)
+	 }
+	 else{
+		results = await fetchApiData(species);
+		if (results.length === 0) {
+		  throw "API returned an empty array";
+		}
+		
+	 }
+	 res.send({
 		fromCache: false,
 		data: results,
 	  });
+	 
 	} catch (error) {
 	  console.error(error);
 	  res.status(404).send("Data unavailable");
